d4b232f6d8d6b055e9868146290da8320f43cc86
chenBenTolila <chenbt266@gmail.com>
2021-07-06 23:36:10 +0300

    adding comments

--- main.py
+++ main.py
@@ -30,67 +30,72 @@ def mullersMethod(p0, p1, p2, f, epsilon, maxIter):
     :param f: The original function
     :param epsilon: The accepted error
     :param maxIter: The max number of iterations that we are willing to accept
-    :return: the amount of the roots found
+    :return: the amount of the roots found in the range [p0, p2]
     """
-    s = "The initial guess:\np0 = " + str(p0) + ", p1 = " + str(p1) + ", p2 = " + str(p2) + "\n"
-    countRoots = 0
-    start = p0
-    end = p2
-    # Check if one of the initial guess is the root
-    flag = False
-    maxTries = 10
-    move = 0
+    s = "The initial guess:\np0 = " + str(p0) + ", p1 = " + str(p1) + ", p2 = " + str(
+        p2) + "\n"  # print the intial guess for the range
+    countRoots = 0  # the number of roots that were found in the range
+    tempRes = []   # a list that will keep
+    start = p0  # will keep the original start point of the range
+    end = p2  # will keep the original end point of the range
+    # Check if one of the initial guesses is a root of the function
+    flag = False  # will save if one of the initial guesses is a root of the function
+    maxTries = 10  # the max number of tries to fix an initial guess in case it's not in the definition area
+    move = 0  # the number of fixes made for a guess
     while move < maxTries:
         try:
             if f(p0) == 0:
                 print(s)
-                print(colored("The root is: x = " + str(p0) + ", f(x) = " + str(f(p0)) + ", found after 0 iterations", 'blue'))
+                print(colored("The root is: x = " + str(p0) + ", f(x) = " + str(f(p0)) + ", found after 0 iterations",
+                              'blue'))
                 countRoots += 1
+                tempRes.append(p0)
                 flag = True
             break
         except:
-            # print("catch p0")
             move += 1
             p0 += min(0.0001, epsilon)
     if maxTries is move:
-        # print("Error - function is undefined in this range :(")
-        return countRoots
+        # print("Error - function is undefined in this range :("
+        return countRoots, tempRes
     move = 0
     while move < maxTries:
         try:
             if f(p1) == 0:
                 print(s)
-                print(colored("The root is: x = " + str(p1) + ", f(x) = " + str(f(p1)) + ", found after 1 iterations", 'blue'))
+                print(colored("The root is: x = " + str(p1) + ", f(x) = " + str(f(p1)) + ", found after 1 iterations",
+                              'blue'))
                 countRoots += 1
+                tempRes.append(p1)
                 flag = True
             break
         except:
-            # print("catch p1")
             move += 1
             p1 += min(0.0001, epsilon)
     if maxTries is move:
         # print("Error - function is undefined in this range :(")
-        return countRoots
+        return countRoots, tempRes
 
     move = 0
     while move < maxTries:
         try:
             if f(p2) == 0:
                 print(s)
-                print(colored("The root is: x = " + str(p2) + ", f(x) = " + str(f(p2)) + ", found after 2 iterations", 'blue'))
+                print(colored("The root is: x = " + str(p2) + ", f(x) = " + str(f(p2)) + ", found after 2 iterations",
+                              'blue'))
                 countRoots += 1
+                tempRes.append(p2)
                 flag = True
             break
         except:
-            # print("catch p2")
             move += 1
             p2 -= min(0.0001, epsilon)
     if maxTries is move:
         # print("Error - function is undefined in this range :(")
-        return countRoots
+        return countRoots, tempRes
 
     if flag is True:
-        return countRoots
+        return countRoots, tempRes
 
     # the iterations
     h1 = p1 - p0
@@ -105,9 +110,10 @@ def mullersMethod(p0, p1, p2, f, epsilon, maxIter):
             if (b ** 2 - (
                     4 * f(p2) * d)) < 0:  # if the discriminant is negative - cant do square root on negative number
                 # print("*** There is no real root ***")
-                return countRoots
+                return countRoots, tempRes
         except:
-            return countRoots
+            # print("overflow")
+            return countRoots, tempRes
         D = (b ** 2 - (4 * f(p2) * d)) ** 0.5  # the square root in the denominator
         if abs(b - D) < abs(b + D):  # Save the biggest result
             E = b + D
@@ -118,21 +124,23 @@ def mullersMethod(p0, p1, p2, f, epsilon, maxIter):
         try:
             f(p)
         except:
-            return countRoots
+            return countRoots, tempRes
         if abs(h) < epsilon:  # if the difference is smaller then epsilon
             # if (p > start) and (p < end):
             if (p > start) and (p < end) and (checkResult(f, p, epsilon) is True):  # if the point is in the range
                 if abs(f(p)) <= 0 + epsilon:
                     print(s, end="")
                     print(
-                        colored("The root is: x = " + str(p) + ", f(x) = " + str(f(p)) + ", found after " + str(iter) + " iterations\n", 'blue'))
+                        colored("The root is: x = " + str(p) + ", f(x) = " + str(f(p)) + ", found after " + str(
+                            iter) + " iterations\n", 'blue'))
                     countRoots += 1
-                    return countRoots
+                    tempRes.append(p)
+                    return countRoots, tempRes
                 else:
-                    return countRoots
+                    return countRoots, tempRes
             else:
                 # print("*** There is no root in this range ***")
-                return countRoots
+                return countRoots, tempRes
         s += ("iteration = " + str(iter) + ", x = " + str(p) + ", f(x) = " + str(f(p)) + "\n")
         # Preparing the variable for the next iteration
         p0 = p1
@@ -145,7 +153,7 @@ def mullersMethod(p0, p1, p2, f, epsilon, maxIter):
         d = (grad2 - grad1) / (h2 + h1)
         iter += 1  # increase the number of iteration
     # print("Method failed after " + str(maxIter) + "iterations")
-    return countRoots
+    return countRoots, tempRes
 
 
 def rangeDivision(polinom, epsilon, iterNumber, maxRoots, start_point, end_point):
@@ -159,41 +167,61 @@ def rangeDivision(polinom, epsilon, iterNumber, maxRoots, start_point, end_point
     """
 
     # dividing the range to small parts
+    results = []
     rootsCount = 0
     end = start_point + 0.1
     middle = start_point + 0.05
     while end <= end_point and rootsCount < maxRoots:  # while we don't reach to the end point of the range
         # print("range: [" + str(start_point) + ", " + str(end) + "]")
-        rootsCount += mullersMethod(start_point, middle, end, polinom, epsilon,
-                                    iterNumber)  # calculate by muller`s method
+        tempCount, tempRes = mullersMethod(start_point, middle, end, polinom, epsilon,
+                                           iterNumber)  # calculate by muller`s method
+        rootsCount += tempCount
+        results.extend(tempRes)
         start_point = end  # increase the start point of the range
         middle = (start_point + 0.05)  # set the middle point of the range
         end = start_point + 0.1  # set the end point of the range
     print("The number of roots found is: " + str(rootsCount))
+    return results
+
+
+
+def checkDifferPart3(l, d, name1, name2, epsilon):
+    print("check the difference between the roots:")
+    # flag = True
+    for i in range(len(l)):
+        print("x" + str(i + 1) + " - " + name1 + ": " + str(l[i])
+              + "\nx" + str(i + 1) + " - " + name2 + ": " + str(d[i]))
+        if abs(l[i] - d[i]) > epsilon:
+            # flag = False
+            print("The difference is bigger than epsilon for some of the components\n")
+            return
+    print("The difference is smaller than epsilon for all the components\n")
 
 
 def driver():
     x = sp.symbols('x')
     f = x ** 4 - 3 * (x ** 3) + x ** 2 + x + 1
     f = (x ** 4 - 4 * (x ** 2)) / x
-    #f = (x ** 3 + 1) ** 15
+    # f = (x ** 3 + 1) ** 15
     # f = ((x - x ** 3) ** 0.5) / ((math.exp(1)) ** x)
     # f = (x) ** 0.5
     # y = sp.lambdify(f, x)
     # print(y)
     # f = (math.exp(1))**x
-    #f = sympy.cos(x)
-    #f = (10 * x * ((math.exp(1) ** x)) - (x ** 3)) / (x**3 - 5)
-    #f = log(x ** 4, 10) + x + 1
-    f = x**2 + 2
+    # f = sympy.cos(x)
+    # f = (10 * x * ((math.exp(1) ** x)) - (x ** 3)) / (x**3 - 5)
+    f = log(x ** 4, 10) + x + 1
     epsilon = 10 ** -5
     maxRoots = 15
     start_point = -100
     end_point = 100
     maxIteration = 100
     f = sp.lambdify(x, f)
-    rangeDivision(f, epsilon, maxIteration, maxRoots, start_point, end_point)
+    results = rangeDivision(f, epsilon, maxIteration, maxRoots, start_point, end_point)
+    checkDifferPart3(results, [0.1, 0.2, 0.3], "Muller's", "Expected", 10 ** -5)
     print("program finished :)")
 
 
 driver()
+
+
